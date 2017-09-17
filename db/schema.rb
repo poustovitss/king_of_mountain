7c1904122740950821cde56bdc2d9c8a8ac89fb4
Sergiy Poustovit <levellevel4@gmail.com>
2017-09-17 21:31:31 +0300

    Add default active field to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170917133909) do
+ActiveRecord::Schema.define(version: 20170917182932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20170917133909) do
     t.string "vk_link"
     t.string "tw_link"
     t.integer "proviant_owner"
+    t.boolean "active", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
